e0203180346c54b9632224da6f063e2e1cf91b55
longbuivan <longbuivan95@gmail.com>
2022-01-20 18:07:33 +0700

    add build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,7 @@ echo "Pull required packages"
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 pip3 install --upgrade pip setuptools wheel
 pip3 install boto3==1.20.10 --user
+source $HOME/.poetry/env && poetry self update && && poetry install --no-root
 
 echo "Verify poetry installing"
+poetry --version
