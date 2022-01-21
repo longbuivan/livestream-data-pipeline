769274dbbc3155f68c183dbb672feb82fc35d5e3
longbuivan <longbuivan95@gmail.com>
2022-01-21 14:35:33 +0700

    fix poetry

--- build.sh
+++ build.sh
@@ -5,8 +5,14 @@ echo "Clean out mirrors and other cache apt things"
 # apt install unixODBC-devel -y
 
 echo "Pull required packages"
+# pip install poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 pip3 install --upgrade pip setuptools wheel
 pip3 install boto3==1.20.10 --user
-source $HOME/.poetry/env && poetry self update && poetry install --no-root && poetry run pytest --cov=src --cov-fail-under=0 tests/
+source "$HOME"/.poetry/env
+# poetry self update 1.0.9
+# poetry init --name "livestream-datapipeline" --version 1.0.0 --description "ingesting weblogs to datalake"
+# poetry init
+# poetry config virtualenvs.create false
+poetry install && poetry run pytest --cov=src --cov-fail-under=0 tests/
 echo "Verify poetry installing"
