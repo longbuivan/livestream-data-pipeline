795dfba2e5dda7bd5a722f501da3a08652cab7a1
longbuivan <longbuivan95@gmail.com>
2022-01-21 15:23:12 +0700

    remove boto error

--- tests/test_parse_json.py
+++ tests/test_parse_json.py
@@ -2,7 +2,6 @@ from unittest.mock import patch, call
 import json
 import base64
 import time
-from botocore.exceptions import ClientError
 import pytest
 from freezegun import freeze_time
 from src.parse_json import lambda_handler
