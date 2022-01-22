92356234ce06be48d06c0b37384d935a6d3bdca2
longbuivan <longbuivan95@gmail.com>
2022-01-22 13:29:06 +0700

    restrict ci run

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -xe
 
 echo "Clean out mirrors and other cache apt things"
-sudo apt-get autoclean
-sudo apt python3-devel.x86_64 -y
-sudo apt install python3-setuptools.noarch -y 
-apt install unixODBC-devel -y
+sudo apt autoclean
+sudo apt autoremove
+# sudo apt python3-devel.x86_64 -y
+# sudo apt install python3-setuptools.noarch -y 
+# sudo apt install unixODBC-devel -y
 
 echo "Preparing environment is done"
