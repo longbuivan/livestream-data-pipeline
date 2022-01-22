3edc33feb414402d2d3c4e7324c839e03487fd0f
longbuivan <longbuivan95@gmail.com>
2022-01-22 12:16:43 +0700

    remove folder

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -xe
 
 echo "Clean out mirrors and other cache apt things"
-# apt clean all
-# apt python3-devel.x86_64 -y
-# apt install python3-setuptools.noarch -y 
+sudo apt clean all
+sudo apt python3-devel.x86_64 -y
+sudo apt install python3-setuptools.noarch -y 
 # apt install unixODBC-devel -y
 
-echo "Pull required packages"
+# echo "Pull required packages"
 # pip install poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
-pip3 install --upgrade pip setuptools wheel
-pip3 install boto3==1.20.10 --user
+# curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+# pip3 install --upgrade pip setuptools wheel
+# pip3 install boto3==1.20.10 --user
 # source "$HOME"/.poetry/bin && poetry install && poetry run pytest --cov=src --cov-fail-under=0 tests/
 echo "Verify poetry installing"
 
