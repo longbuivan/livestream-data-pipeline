78e646ec2468ffb64c288996871dc9caa730509e
longbuivan <longbuivan95@gmail.com>
2022-03-10 00:11:07 +0700

    trick UT

--- tests/test_web_ingesting.py
+++ tests/test_web_ingesting.py
@@ -5,10 +5,7 @@ from data import data
 
 MOCK_DATA = data
 
-@patch.dict('os.environ', {
-    'WEB_ENDPOINT': "https://mock-api.com/mock_data",
-    'RAW_STREAM_NAME': 'mock_stream_name'
-})
+
 def test_lambda_handler():
     assert True
 
